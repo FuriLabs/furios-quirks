d82620ddc7d70ab2eab1dea3938298134dead674
Bardia Moshiri <fakeshell@bardia.tech>
2024-03-22 12:01:12 -0400

    hybris-gl: set GSK_RENDERER to gl
    latest gtk sets default to ngl which is still buggy and slow
    
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- hybris-gl/zz-droidian.sh
+++ hybris-gl/zz-droidian.sh
@@ -6,6 +6,9 @@ export COGL_DISABLE_MAPBUFFERRANGE=true
 # Force gles on gdk
 export GDK_GL=gles
 
+# Force gl backend instead of ngl
+export GSK_RENDERER=gl
+
 # Disable webkitgtk bwrap sandbox for now
 export WEBKIT_FORCE_SANDBOX=0
 export WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1
