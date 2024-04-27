375b65d09c4aeb795c31f164b4eb85101b0a7b07
Bardia Moshiri <fakeshell@bardia.tech>
2024-04-27 13:01:22 -0400

    gnome: drop qt style override
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- gnome/zz-gnome-vars.sh
+++ gnome/zz-gnome-vars.sh
@@ -4,7 +4,3 @@
 export XDG_CURRENT_DESKTOP=Phosh:GNOME
 export XDG_SESSION_DESKTOP=phosh
 export XDG_MENU_PREFIX="gnome-"
-
-# make qt apps more gnome/gtk like
-export QT_QPA_PLATFORMTHEME='gnome'
-export QT_STYLE_OVERRIDE=adwaita
